3df4babb181fb6935a4c43bc7c75a13b11a14bd8
Kenny Cox <thisisnotkenn@gmail.com>
2019-02-27 23:37:19 -0700

    Updated Arduino code to print the correct packet for Phaser

--- TechProtoType/Technical_Prototype_Kenny/Technical_Prototype_Kenny.ino
+++ TechProtoType/Technical_Prototype_Kenny/Technical_Prototype_Kenny.ino
@@ -17,29 +17,33 @@ void loop() {
   
   // read in flex sensor 
   flexVal = (flexVal + analogRead(indexFlex)) / 2; // moving average 
-  Serial.print("Flex Value: ");
-  Serial.println(flexVal);
+//  Serial.print("Flex Value: ");
+ // Serial.println(flexVal);
   lightVal = map(flexVal, 775, 1023, 0, 255);
   analogWrite(red,lightVal);
 
   // read in finger placement
   palmVal = analogRead(indexPalm);
-  Serial.print(" | Palm Value: ");
-  Serial.println(palmVal);
+ // Serial.print(" | Palm Value: ");
+//  Serial.println(palmVal);
   if(palmVal < 800) // open finger
   {
     digitalWrite(yellow, LOW);
     digitalWrite(green, LOW);
+    Serial.print("0");
   }
   if(palmVal >= 800 && palmVal < 900) // cat paw
   {
     digitalWrite(yellow, HIGH);
     digitalWrite(green, LOW);
+    Serial.print("2");
   }
   if(palmVal >= 900 )
   {
     digitalWrite(yellow, LOW);
     digitalWrite(green, HIGH);
+    Serial.print("1");
   }
+  Serial.print("-");
   delay(20);
 }
